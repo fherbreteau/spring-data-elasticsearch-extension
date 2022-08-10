b302cb44af1be31883959e8ad38e57ba1bc3954c
Francois HERBRETEAU <francois.herbreteau@rte-france.com>
2022-08-10 10:50:22 +0200

    Full rework of the extended search. Improve Coverage required.

--- src/main/java/org/springframework/data/elasticsearch/core/AbstractExtendedSearchTemplate.java
+++ src/main/java/org/springframework/data/elasticsearch/core/AbstractExtendedSearchTemplate.java
@@ -31,8 +31,7 @@ import java.util.stream.Collectors;
 public abstract class AbstractExtendedSearchTemplate implements ExtendedSearchOperations, ApplicationContextAware {
 
     protected final ElasticsearchConverter elasticsearchConverter;
-    @Nullable
-    protected EntityCallbacks entityCallbacks;
+    @Nullable protected EntityCallbacks entityCallbacks;
 
     protected AbstractExtendedSearchTemplate() {
         this(null);
